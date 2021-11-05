cd1d38788ca66579cd1a8204d893ae7a051fcf7a
Samuel Mei <Samoooomei@gmail.com>
2021-11-05 03:56:13 -0400

    Basic Form

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
 	<React.StrictMode>
