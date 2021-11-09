708282797c898d23b2833589355c689151f51731
Samuel Mei <43279636+SamooMei@users.noreply.github.com>
2021-11-09 00:59:20 -0500

    Dependencies and Login (#2)
    * Added Router & Login
    
    * Basic Form
    
    * Styling
    
    * Wrong Deps Removed
    
    * Register Works
    
    * Debuggery

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import App from './App'
+import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import Register from './components/Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Router>
+			<Switch>
+				<Route exact path="/register" component={Register} />
+			</Switch>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
