ddd7712697bf752524b0e07c9defeaaa17499579
unknown <Samoooomei@gmail.com>
2021-11-18 11:24:31 -0500

    Fixed Navbar Changes

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -9,31 +9,14 @@ const Home = () => {
   const [user, setUsername] = useState("");
 
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    fetch('http://localhost:5000/api/users', {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({username: `${user}`})
-    })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
-}
+  const handleSubmit = (e) => {};
 
 
   return (
     <div className={"Home"}>
       <img src={Logo} alt="" id={'Logo'}/>
       <div className={"text"}> RPI MARKETPLACE </div>
-      <div className={"text"}> A marketplace for students, by students </div>
+      <div className={"text"}> "A marketplace for students, by students" </div>
       <div>
         <Form className={"Home__Form"}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,17 +3,15 @@ import './index.scss';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => (
-    <header className="navbar">
-        <nav className="navbar_navigation">
+    <div className="navbar">
+        <nav className="navbar__navigation">
             <div></div>
-            <div className="navBar_logo"><Link to="/">Marketplace</Link></div>
-            <div className="spacer"></div>
-            <div className="navbar_items">
-                <ul>
-                    <li className="Item1"><Link to="/register">Sign In</Link></li>
-                </ul>
+            <div className="navbar__navigation__title"><Link className="navbar__navigation__link" to="/">Marketplace</Link></div>
+            <div className="navbar__navigation__spacer"></div>
+            <div className="navbar__navigation__items">
+                <Link className="navbar__navigation__link"to="/register">Sign In</Link>
             </div>
         </nav>
-    </header>
+    </div>
 );
 export default NavBar;
